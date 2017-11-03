1d91c1a19d52f58b6b3e0a0bbe31ba378f0ef540
Vextrom <ddor001@ucr.edu>
2017-11-03 04:01:18 -0700

    Made progress on implementing connectors.

--- src/parsing.cpp
+++ src/parsing.cpp
@@ -1,2 +1,49 @@
 #include "parsing.h"
+using namespace std;
+
+void Parsing :: setLine(char* userLine)
+{
+    line = userLine;
+}
+
+void Parsing::runLine()
+{
+	int i = 0;
+	char *copy = strdup(line);
+	char *connectors = ";|&";
+	char *token = strtok(line, connectors);
+	while (token)
+	{
+		char a = copy[token - line + strlen(copy)];
+	}
+
+	do
+	{
+		buildCommand();
+	} while (i > listOfConnectors.size(), ++i);
+
+}
+
+bool Parsing :: exitExists()
+{
+	return exitState;
+}
+
+void Parsing::buildCommand()
+{	
+	char* token = strtok(line, " ");
+	string commandName = token;
+	string args;
+	while (token != NULL)
+	{
+		token = strtok(NULL, "|&;");
+		if (token != NULL)
+		{
+			args = token;
+			break;
+		}
+	}
+	command * newcmd = new command(commandName, args);
+	listOfCommands.push_back(newcmd);
+}
 
