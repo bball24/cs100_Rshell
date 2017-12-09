2f4bc30b143581fcd0cf00efc2a0d4105ce65c1e
Vextrom <ddor001@ucr.edu>
2017-12-08 21:48:19 -0800

    Implemented piping.

--- src/command.cpp
+++ src/command.cpp
@@ -3,147 +3,172 @@
 // Attempts to run the given command from /usr/bin
 int command :: executeCommand()
 {
-	// Check to see if test command, use other function if that's the case
-	if(commandName == "test")
-	{
-		if (arguments == "")
-		{
-			cout << "No arguments supplied for test" << endl;
-			return false;
-		}
-		return !testCommand();;
-	}
-	// PID is set to 0 for the child process. PID is set to the PID of the child process, in the parent process.
-	pid_t pid;
-	int status;
-	int execReturn = 0;
-	pid = fork();
-	// If we aren't in the child process or parent process there was a problem with fork().
-	if (pid < 0)
-	{
-		cout << "Error with fork()" << endl;
-		exit(-1);
-	}
-	// Runs the executable in the child process, so we don't overwrite the
-	// parent process, and lose the ability to return to main.
-	else if (pid == 0)
-	{
-		// Need to make a char** array to use execvp.
-		// Essentially we're filling it with the command name and its arguments.
-		string nameAndArgs = commandName + ' ' + arguments;
-		char* stringForToken = new char [nameAndArgs.size()];
-		strcpy(stringForToken, nameAndArgs.c_str());
-		// Get the name of the command to store it in arg[0].
-		char* j = strtok (stringForToken," ");
-		// Iterator for filling arg[]
-		int k = 0; // Iterator
-		// Allocate the data for arg
-		char** arg = new char*[commandName.length()+1]; // Allocating data
-		// Fill arg[]
-		while(j != NULL)
-		{
-			arg[k] = j;
-			j = strtok(NULL, " ");
-			++k;
-		}
-		arg[k] = NULL;
-		execReturn = execvp(arg[0], arg);
-		delete[] stringForToken;
-		delete[] arg;
-		if( execReturn < 0 )
-		{
-			perror( "execvp Failed" );
-		}
-		// Return whether the command failed or succeeded.
-		exit(execReturn);
-	}
-	else
-	{
-		// Get the exit status from the child process and then return it.
-		while (wait(&status) != pid);
-		return status;
-	}
-	return -1;
+    // PID is set to 0 for the child process. PID is set to the PID of the child process, in the parent process.
+    pid_t pid;
+    int status;
+    int execReturn = 0;
+    pid = fork();
+    // If we aren't in the child process or parent process there was a problem with fork().
+    if (pid < 0)
+    {
+        cout << "Error with fork()" << endl;
+        exit(-1);
+    }
+    // Runs the executable in the child process, so we don't overwrite the
+    // parent process, and lose the ability to return to main.
+    else if (pid == 0)
+    {
+        // Need to make a char** array to use execvp.
+        // Essentially we're filling it with the command name and its arguments.
+        string nameAndArgs = commandName + ' ' + arguments;
+        char* stringForToken = new char [nameAndArgs.size()];
+        strcpy(stringForToken, nameAndArgs.c_str());
+        // Get the name of the command to store it in arg[0].
+        char* j = strtok (stringForToken," ");
+        // Iterator for filling arg[]
+        int k = 0; // Iterator
+        // Allocate the data for arg
+        char** arg = new char*[commandName.length()+1]; // Allocating data
+        // Fill arg[]
+        while(j != NULL)
+        {
+            arg[k] = j;
+            j = strtok(NULL, " ");
+            ++k;
+        }
+        arg[k] = NULL;
+        execReturn = execvp(arg[0], arg);
+        delete[] stringForToken;
+        delete[] arg;
+        if( execReturn < 0 )
+        {
+            perror( "execvp Failed" );
+        }
+        // Return whether the command failed or succeeded.
+        exit(execReturn);
+    }
+    else
+    {
+        // Get the exit status from the child process and then return it.
+        while (wait(&status) != pid);
+        return status;
+    }
+    return -1;
 }
 
 string command::getCommandName()
 {
-	return commandName;
+    return commandName;
 }
 
-int command::testCommand()
+char** command::commandForPipe()
 {
-	int k = 0;
-	char* testArguments = new char [arguments.size()];
-	strcpy(testArguments, arguments.c_str());
-	char* j = strtok (testArguments, " ");
-	char** args = new char*[arguments.length()+1];
-	char* path;
-	string flagd = "-d";
-	string flage = "-e";
-	string flagf = "-f";
-	while(j != NULL)
-	{
-		args[k] = j;
-		j = strtok(NULL, " ");
-		++k;
-	}
-    struct stat fileBuffer;
-    string flag;
-	if (args[0] != flagd.c_str() || args[0] != flage.c_str() || args[0] != flagf.c_str())
-	{
-		flag = args[0];
-		path = args[1];
-	}
-	else
-	{
-		path = args[0];
-	}
-    if(stat(path, &fileBuffer) == -1)
+    string nameAndArgs = commandName + ' ' + arguments;
+    char* stringForToken = new char [nameAndArgs.size()];
+    strcpy(stringForToken, nameAndArgs.c_str());
+    char* j = strtok (stringForToken," ");
+    int k = 0;
+    char** arg = new char*[commandName.length()+1];
+    while(j != NULL)
     {
-        perror("Stat Error");
-        return false;
+        arg[k] = j;
+        j = strtok(NULL, " ");
+        ++k;
     }
-    if (flag != "-e" && flag != "-d" && flag != "-f") // default flag
-    {
-        flag = "-e";
-    }
+    arg[k] = NULL;
+    return arg;
+}
 
-    if(flag == "-e") // e flag
+int connector::executeCommand()
+{
+    if(connectorChar == ';')
     {
-		cout << "(True)" << endl;
-		return true;
- 
+        leftCommand->executeCommand();
+        return rightCommand->executeCommand();
     }
-    if(flag == "-d") // d flag
+    else if (connectorChar == '&')
     {
-        int checkDirectory = S_ISDIR(fileBuffer.st_mode);
-        if(checkDirectory)
+        int left = leftCommand->executeCommand();
+        if (left == 0)
         {
-             cout << "(True)" << endl;
+            return rightCommand->executeCommand();
+        }
+        return left;
+    }
+    else if (connectorChar == '|')
+    {
+        int left = leftCommand->executeCommand();
+        if (left != 0)
+        {
+            return rightCommand->executeCommand();
+        }
+        return left;
+    }
+    else
+    {
+        perror("Invalid connector used");
+        return 0;
+    }
+    return 0;
+};
+
+void connector::setLeftCommand(base* cmd)
+{
+    leftCommand = cmd;
+}
+
+void connector::setRightCommand(base *cmd)
+{
+    rightCommand = cmd;
+}
+
+void pipeCmd::setLeftCommand(command *cmd)
+{
+    leftCommand = cmd;
+}
+
+void pipeCmd::setRightCommand(command *cmd)
+{
+    rightCommand = cmd;
+}
+
+int pipeCmd::executeCommand()
+{
+    int WRITE_END = 1;
+    int READ_END = 0;
+    char** cmd1;
+    char** cmd2;
+    cmd1 = leftCommand->commandForPipe();
+    cmd2 = rightCommand->commandForPipe();
+    int fds[2];
+    pipe(fds);
+    pid_t pid = fork();
+    if(pid == 0)
+    {
+        dup2(fds[WRITE_END], STDOUT_FILENO);
+        close(fds[READ_END]);
+        close(fds[WRITE_END]);
+        execvp(cmd1[0], cmd1);
+        exit(1);
+    }
+    else
+    {
+        pid = fork();
+
+        if(pid == 0)
+        {
+            dup2(fds[READ_END], STDIN_FILENO);
+            close(fds[WRITE_END]);
+            close(fds[READ_END]);
+            execvp(cmd2[0], cmd2);
+            exit(1);
         }
         else
         {
-             cout << "(False)" << endl;
+            int status;
+            close(fds[READ_END]);
+            close(fds[WRITE_END]);
+            waitpid(pid, &status, 0);
         }
-        return checkDirectory; // valid command
     }
-    if(flag == "-f")
-    {
-        int isRegFile = S_ISREG(fileBuffer.st_mode);
-        if(isRegFile)
-        {
-            cout << "(True)" << endl;
-        }
-        else
-        { 
-            cout << "(False)" << endl;
-        }
-        
-        return isRegFile; // valid command
-    }
-
-
-
-     return false;
 }
--- src/command.h
+++ src/command.h
@@ -3,29 +3,59 @@
 #include <iostream>
 #include <string>
 #include <sys/wait.h>
-#include <sys/stat.h>
 #include <unistd.h>
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
-#include <vector>
 using namespace std;
 
-class command
+class base
 {
-	private:
-		// Stores the name of the command
-		string commandName;
-		// Stores the arguments respective to the given command.
-		string arguments;
-	public:
-		//Constructors
-		command() : commandName(), arguments() { };
-		command(string comm, string args) : commandName(comm), arguments(args) { };
-		// Attempts to run the given command from /usr/bin
-		int executeCommand();
-		string getCommandName();
-        int testCommand();
+    public:
+    base () { }
+    virtual int executeCommand() = 0;
+};
+
+class command : public base
+{
+    private:
+        // Stores the name of the command
+        string commandName;
+        // Stores the arguments respective to the given command.
+        string arguments;
+    public:
+        // Constructors
+        command() : commandName(), arguments() { }
+        command(string comm, string args) : commandName(comm), arguments(args) { }
+        // Attempts to run the given command from /usr/bin
+        int executeCommand();
+        string getCommandName();
+        char** commandForPipe();
+};
+
+class connector : public base
+{
+    // This uses a structure similar to what was used in the
+    // composite + strategy lab.
+    private:
+        char connectorChar;
+        base* leftCommand;
+        base* rightCommand;
+    public:
+        int executeCommand();
+        void setLeftCommand(base* cmd);
+        void setRightCommand(base* cmd);
+};
+
+class pipeCmd : public base
+{
+    private:
+        command* leftCommand;
+        command* rightCommand;
+    public:
+        int executeCommand();
+        void setLeftCommand(command* cmd);
+        void setRightCommand(command* cmd);
 };
 
 
