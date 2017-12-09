dff2004dcfcc9a1855ebade57d9a1d0e8af1b060
bball24 <jerry88tan@gmail.com>
2017-12-08 23:40:02 -0800

    updated interface

--- src/command.h
+++ src/command.h
@@ -3,30 +3,102 @@
 #include <iostream>
 #include <string>
 #include <sys/wait.h>
-#include <sys/stat.h>
 #include <unistd.h>
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
-#include <vector>
 using namespace std;
 
-class command
+class base
+{
+    public:
+    base () { }
+    virtual int executeCommand() = 0;
+};
+
+class command : public base
+{
+    private:
+        // Stores the name of the command
+        string commandName;
+        // Stores the arguments respective to the given command.
+        string arguments;
+    public:
+        // Constructors
+        command() : commandName(), arguments() { }
+        command(string comm, string args) : commandName(comm), arguments(args) { }
+        // Attempts to run the given command from /usr/bin
+        int executeCommand();
+        string getCommandName();
+        char** commandForPipe();
+};
+
+class connector : public base
+{
+    // This uses a structure similar to what was used in the
+    // composite + strategy lab.
+    private:
+        char connectorChar;
+        base* leftCommand;
+        base* rightCommand;
+    public:
+        int executeCommand();
+        void setLeftCommand(base* cmd);
+        void setRightCommand(base* cmd);
+};
+
+class pipeCmd : public base
+{
+    private:
+        command* leftCommand;
+        command* rightCommand;
+    public:
+        int executeCommand();
+        void setLeftCommand(command* cmd);
+        void setRightCommand(command* cmd);
+};
+
+class inputRedirect :: public base
 {
 	private:
-		// Stores the name of the command
-		string commandName;
-		// Stores the arguments respective to the given command.
-		string arguments;
+		
+	base * leftCommand;
+	base * rightCommand;
+
 	public:
-		//Constructors
-		command() : commandName(), arguments() { };
-		command(string comm, string args) : commandName(comm), arguments(args) { };
-		// Attempts to run the given command from /usr/bin
 		int executeCommand();
-		string getCommandName();
-        int testCommand();
+		void setLeftCommand(base * cmd);
+		void setRightCommand(base * cmd);
+
+
 };
+class OutputRedirect :: public base
+{
+private:
+		
+	base * leftCommand;
+	base * rightCommand;
+
+	public:
+		int executeCommand();
+		void setLeftCommand(base * cmd);
+		void setRightCommand(base * cmd);
+};
+
+class appendRedirect :: public base
+{
+private:
+	
+	base * leftCommand;
+	base * rightCommand;
+
+	public:
+		int executeCommand();
+		void setLeftCommand(base * cmd);
+		void setRightCommand(base * cmd);
+};
+
+
 
 
 #endif
